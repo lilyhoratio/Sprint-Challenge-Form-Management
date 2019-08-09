5e8abcae0c1b8a6c5642dc576065f3786b893c80
lilyhoratio <lilyhzhou@gmail.com>
2019-08-09 09:19:01 -0700

    created form.test.js and moved state to app

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,15 +1,30 @@
 import React from "react";
-import { Form, Field } from "formik";
+import { Form, Field, withFormik } from "formik";
 
 class FormComp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     users: []
+  //   };
+  // }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   console.log("prev props:", prevProps)
+  //   console.log("current props:", this.state.props)
+  // }
 
   render() {
-    return <Form>hi</Form>;
+    return (
+      <Form>
+        <Field type="text" name="username" placeholder="username" />
+        <Field type="password" name="password" placeholder="password" />
+        <button type="submit">Submit</button>
+      </Form>
+    );
   }
 }
 
-export default FormComp;
+const FormikForm = withFormik({})(FormComp);
+
+export default FormikForm;
