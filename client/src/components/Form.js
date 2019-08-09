6078f2c3298e59286a662e205c5f69c2c585418d
lilyhoratio <lilyhzhou@gmail.com>
2019-08-09 09:30:18 -0700

    use yup for validation

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,30 +1,48 @@
 import React from "react";
 import { Form, Field, withFormik } from "formik";
+import * as yup from "yup";
 
-class FormComp extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     users: []
-  //   };
-  // }
+// class FormComp extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       users: []
+//     };
+//   }
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   console.log("prev props:", prevProps)
-  //   console.log("current props:", this.state.props)
-  // }
+const FormComp = props => {
+  // render() {
+  console.log(props.values);
+  console.log(props.errors);
+  return (
+    <Form>
+      {props.touched.username && props.errors.username && (
+        <p className="error">{props.errors.username}</p>
+      )}
+      <Field type="text" name="username" placeholder="username" />
+      {props.touched.password && props.errors.password && (
+        <p className="error">{props.errors.password}</p>
+      )}
+      <Field type="password" name="password" placeholder="password" />
+      <button type="submit">Submit</button>
+    </Form>
+  );
+};
 
-  render() {
-    return (
-      <Form>
-        <Field type="text" name="username" placeholder="username" />
-        <Field type="password" name="password" placeholder="password" />
-        <button type="submit">Submit</button>
-      </Form>
-    );
-  }
-}
-
-const FormikForm = withFormik({})(FormComp);
+const FormikForm = withFormik({
+  mapPropsToValues: ({ username, password }) => {
+    return {
+      username: username || "",
+      password: password || ""
+    };
+  },
+  validationSchema: yup.object().shape({
+    username: yup.string().required("Username is required."),
+    password: yup
+      .string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Password is required.")
+  })
+})(FormComp);
 
 export default FormikForm;
