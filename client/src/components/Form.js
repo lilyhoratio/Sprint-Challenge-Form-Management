43404b790b12a7975274949dd66b732d60ee3313
lilyhoratio <lilyhzhou@gmail.com>
2019-08-09 08:31:38 -0700

    initial set up

---
+++ client/src/components/Form.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Form, Field } from "formik";
+
+class FormComp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <Form>hi</Form>;
+  }
+}
+
+export default FormComp;
